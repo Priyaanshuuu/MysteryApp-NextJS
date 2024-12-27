e15f2889949092eeb2a1fa223d2f20361550faef
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2024-12-27 20:01:24 +0530

    AuthJs setup

---
+++ annonymousmessagingapp/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,6 @@
+import { NextAuthOptions} from "next-auth";
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import bcrypt from "bcrypt"
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+
