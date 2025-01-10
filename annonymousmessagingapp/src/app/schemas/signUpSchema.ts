8c6d26bcd8509bdb5675195ec6cb7163938e2d4f
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-01-10 15:29:28 +0530

    zod

--- annonymousmessagingapp/src/app/schemas/signUpSchema.ts
+++ annonymousmessagingapp/src/app/schemas/signUpSchema.ts
@@ -11,3 +11,8 @@ export const signUpSchema = z.object({
     email: z.string().email({message: 'Invalid email address'}),
     password: z.string().min(6,"password") 
 })
+// zod is nothing but a validation library. It is used to check whether the data coming meets our requirements or not
+// Mongoose can also be used for the schema validations but here zod is used because it doesn;t require database
+// zod is an application level based validation feature i.e it validates from the frontend and the backend
+// In this project the data will come through API so zod is prefered over mongoose
+// use mongoose whenever database validation is required
