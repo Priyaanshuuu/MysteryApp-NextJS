6a3719622db0ed5d1e7a23e7c99c08c174eb13ea
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-01-15 20:18:32 +0530

    DBConnection

--- annonymousmessagingapp/src/lib/dbConnect.ts
+++ annonymousmessagingapp/src/lib/dbConnect.ts
@@ -4,10 +4,15 @@ import mongoose from "mongoose";
 type ConnectionObject = {
     isConnected?: number
 }
+//type is a ts property that define what type of data we're putting here
+// This property is optional as ? is used
+// It may use isConnected as the type or may not
 
 const connection : ConnectionObject= {}
+//created an object named connection which uses ConnectionObject as the type 
 
 async function dbConnect(): Promise<void> {
+    // Promise<void> this means that here no value is returned only actions or tasks will be returned
     if(connection.isConnected){
         console.log("Already connected to database");
         return
@@ -17,6 +22,7 @@ async function dbConnect(): Promise<void> {
         )
         connection.isConnected = db.connections[0].
         readyState
+        //readystate tells us whether the connection is established or not by showing 1 or 0 respectively
 
         console.log("DB is connected successfully");
         
@@ -25,6 +31,7 @@ async function dbConnect(): Promise<void> {
     console.log("Database connection failed",error);
 
     process.exit(1)
+    // process.exit means when failure hits this will exit the project by showing the error as 1 
     }
 }
 
