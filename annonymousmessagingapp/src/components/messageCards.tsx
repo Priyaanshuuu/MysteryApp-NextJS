2a29761f8cd0763e67f012d72d354bf9e4093fb5
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-02-26 15:19:03 +0530

    some ui changes

--- annonymousmessagingapp/src/components/messageCards.tsx
+++ annonymousmessagingapp/src/components/messageCards.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import {
     Card,
-    CardContent,
     CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from './ui/button'
 
 
 function messageCards() {
@@ -14,11 +26,28 @@ function messageCards() {
         <Card>
             <CardHeader>
                 <CardTitle>Card Title</CardTitle>
+                <AlertDialog>
+                    <AlertDialogTrigger><Button variant = "destructive">
+                        
+                        </Button>
+                        </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                This action cannot be undone. This will permanently delete your account
+                                and remove your data from our servers.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
                 <CardDescription>Card Description</CardDescription>
             </CardHeader>
-            <CardContent>
 
-            </CardContent>
             <CardFooter>
             </CardFooter>
         </Card>
