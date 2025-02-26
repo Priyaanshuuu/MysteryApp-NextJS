2b8b252434ffa1d48808625ae1c301ad81686e17
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-02-26 15:13:31 +0530

    messageCard

---
+++ annonymousmessagingapp/src/components/messageCards.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+
+function messageCards() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+
+            </CardContent>
+            <CardFooter>
+            </CardFooter>
+        </Card>
+
+    )
+}
+
+export default messageCards
