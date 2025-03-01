94b3e547668684d9a8712f4f17950534e967ca9f
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-03-01 14:55:51 +0530

    DashBoardSetup

--- annonymousmessagingapp/src/app/dashboard/page.tsx
+++ annonymousmessagingapp/src/app/dashboard/page.tsx
@@ -1,6 +1,28 @@
-import React from 'react'
+'use client'
+import { Message } from '@/model/User.model'
+import  { useState } from 'react'
+import {useToast} from '@/components/ui/toast'
+import { useSession } from 'next-auth/react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { AcceptMessageSchema } from '../schemas/acceptMessageSchema'
 
-function page() {
+const page = () => {
+  const [messages, setMessages] = useState<Message[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSwitchLoading, setIsSwitchingLoading] = useState(false)
+  const toast = useToast()
+
+  const handleDeleteMessage = (messageId: string)=>{
+    setMessages(messages.filter(message=>message.id !== messageId))
+
+    const {data: session} = useSession()
+
+    const form = useForm({
+      resolver: zodResolver(AcceptMessageSchema)
+    })
+
+  }
   return (
     <div>
       The dashboard page
